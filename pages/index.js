ae536154d29fac230e9db3dfa0430d81faf640eb
Beatriz Moura <biamouras@gmail.com>
2023-09-15 15:26:42 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Vitchinho, eu quero você, gatchinho. Se você me ama, vem aqui na sala e imita o Bruninho.</h1>
+    return <h1>Vitchinho, eu quero você, gatchinho. Se você me ama, vem aqui na sala e imita o Bruninho. 😼</h1>
 }
 
 export default Home;
