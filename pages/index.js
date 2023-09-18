6531aee1e234b436796b4676250f0d1a989b364f
Beatriz Moura <biamouras@gmail.com>
2023-09-18 18:23:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Vitchinho, eu quero você, gatchinho. Se você me ama, vem aqui na sala e imita o Bruninho. 😼</h1>
+  return (
+    <h1>
+      Vitchinho, eu quero você, gatchinho. Se você me ama, vem aqui na sala e
+      imita o Bruninho. 😼
+    </h1>
+  );
 }
 
 export default Home;
